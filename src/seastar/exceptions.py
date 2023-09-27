92b15fdefddae3c06627a6fc252009558a6f8b61
Ryan Kroon <rykroon.tech@gmail.com>
2023-09-27 12:08:25 -0400

    Renamed content back to body. Renamed detail back to message.

--- src/seastar/exceptions.py
+++ src/seastar/exceptions.py
@@ -6,10 +6,10 @@ from typing import Any, Optional
 @dataclass
 class HttpException(Exception):
     status_code: int
-    detail: Optional[Any] = None
+    message: Optional[Any] = None
     headers: Optional[dict[str, str]] = None
 
     def __post_init__(self):
-        if not self.detail:
-            self.detail = HTTPStatus(self.status_code).phrase
-        super().__init__(self.status_code, self.detail)
+        if not self.message:
+            self.message = HTTPStatus(self.status_code).phrase
+        super().__init__(self.status_code, self.message)
--- src/seastar/responses.py
+++ src/seastar/responses.py
@@ -1,12 +1,18 @@
 from dataclasses import dataclass
 from typing import Any, Optional
 
+from seastar.datastructures import MutableHeaders
+
 
 @dataclass
 class Response:
-    body: Any
+    body: Any = None
     status_code: Optional[int] = None
-    headers: Optional[dict[str, str]] = None
+    headers: Optional[MutableHeaders[str, str]] = None
+
+    def __post_init__(self):
+        if self.headers is not None and not isinstance(self.headers, MutableHeaders):
+            self.headers = MutableHeaders(self.headers)
 
     def __call__(self):
         result = {}
@@ -16,7 +22,19 @@ class Response:
         if self.status_code is not None:
             result["statusCode"] = self.status_code
 
-        if self.headers is not None:
+        if self.headers:
             result["headers"] = self.headers
 
         return result or None
+
+
+class HtmlResponse(Response):
+    ...
+
+
+class PlainTextResponse(Response):
+    ...
+
+
+class JsonResponse(Response):
+    ...
