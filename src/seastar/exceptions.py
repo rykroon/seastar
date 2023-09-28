90fae8674af31fd2b364cadb7ea9f8c2ef82bedb
Ryan Kroon <rykroon.tech@gmail.com>
2023-09-28 14:03:31 -0400

    headers in HttpException do not need to be case insensitive.

--- src/seastar/exceptions.py
+++ src/seastar/exceptions.py
@@ -3,8 +3,6 @@ from dataclasses import dataclass, field
 from http import HTTPStatus
 from typing import Any, Optional
 
-from seastar.datastructures import MutableHeaders
-
 
 @dataclass
 class HttpException(Exception):
@@ -15,8 +13,5 @@ class HttpException(Exception):
     def __post_init__(self):
         if not self.message:
             self.message = HTTPStatus(self.status_code).phrase
-
-        if not isinstance(self.headers, MutableHeaders):
-            self.headers = MutableHeaders(self.headers)
 
         super().__init__(self.status_code, self.message)
