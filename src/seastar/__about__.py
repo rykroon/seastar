a1dd931837c3dbdd940148dfd0e19d6c4b436e4b
Ryan Kroon <rykroon.tech@gmail.com>
2023-10-05 13:30:29 -0400

    Removed comment.

--- src/seastar/__about__.py
+++ src/seastar/__about__.py
@@ -2,11 +2,3 @@
 #
 # SPDX-License-Identifier: MIT
 __version__ = "0.2.0"
-
-
-"""
- ideas
- if exception middleware is part of the stack then make a note of that
- in the event dict. in lower level apps, like Routes and endpoints, they should
- return a valid response if excpetion middleware is not part of stack.
-"""
