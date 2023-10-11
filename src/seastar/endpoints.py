2f528e0a4c359d31b29428a9e4417ed6177697b0
Ryan Kroon <rykroon.tech@gmail.com>
2023-10-11 19:22:57 -0400

    Added logic for entry_point.

--- src/seastar/endpoints.py
+++ src/seastar/endpoints.py
@@ -1,13 +1,17 @@
+from dataclasses import dataclass, field
 from typing import Optional
 
 from seastar.exceptions import HttpException
 from seastar.requests import Request
+from seastar.responses import PlainTextResponse
 from seastar.types import Event, Context, HandlerResult, RequestHandler
 
 
+@dataclass(eq=False)
 class HttpEndpoint:
+    allowed_methods: list[str] = field(init=False)
 
-    def __init__(self) -> None:
+    def __post_init__(self) -> None:
         self.allowed_methods = [
             method
             for method in ("GET", "HEAD", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
@@ -16,10 +20,18 @@ class HttpEndpoint:
 
     def __call__(self, event: Event, context: Context) -> HandlerResult:
         assert "http" in event, "Expected a web event."
+        is_entry_point = (
+            event.setdefault("__seastar", {}).setdefault("entry_point", self) is self
+        )
         handler: Optional[RequestHandler] = getattr(self, event["http"]["method"].lower(), None)
         if handler is None:
             headers = {"Allow": ", ".join(self.allowed_methods)}
-            raise HttpException(405, headers=headers)
+            if is_entry_point:
+                return PlainTextResponse(
+                    "Method Not Allowed", status_code=405, headers=headers
+                )
+            else:
+                raise HttpException(405, headers=headers)
 
         request = Request.from_event(event)
         response = handler(request)
