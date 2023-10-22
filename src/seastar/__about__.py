cfceaec479e3e8eead2b80f82a81ef572d2cb96f
Ryan Kroon <rykroon.tech@gmail.com>
2023-10-22 10:50:43 -0400

    Added notes in about.

--- src/seastar/__about__.py
+++ src/seastar/__about__.py
@@ -2,3 +2,16 @@
 #
 # SPDX-License-Identifier: MIT
 __version__ = "0.4.0"
+
+
+"""
+Ideas
+
+- Differentiate between a WebRoute and an EventRoute
+    - matches() raises NotImplementedError on BaseRoute
+
+- Exception Middleware can accept a is_web flag.
+    - This will allow the exception middleware to be used for non-web events.
+
+
+"""
